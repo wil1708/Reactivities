7819a97f701a30074920629fa08eb81955bd2a49
wil1708 <william.wpires@gmail.com>
2023-04-27 07:41:59 +0200

    End of section 20

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,6 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity, ActivityFormValues } from "../models/activity";
-import { v4 as uuid } from 'uuid';
 import { format } from 'date-fns';
 import { store } from "./store";
 import { Profile } from "../models/profile";
@@ -177,6 +176,17 @@ export default class ActivityStore {
 
     clearSelectedActivity = () => {
         this.selectedActivity = undefined;
+    }
+
+    updateAttendeeFollowing = (username: string) => {
+        this.activityRegistry.forEach(activity => {
+            activity.attendees.forEach(attendee => {
+                if (attendee.username === username) {
+                    attendee.following ? attendee.followersCount-- : attendee.followersCount++;
+                    attendee.following = !attendee.following;
+                }
+            })
+        })
     }
 
 }
